f60c4d18b1f396072130210228f245a52d553bfe
Graham Campbell <graham@alt-three.com>
2016-11-14 11:41:38 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import client
+import random
 import sys
 
 
@@ -16,4 +17,23 @@ def main(args):
 
 if __name__ == "__main__":
     main(sys.argv)
+
+
+
+def my_move(state):
+    """
+    Put your Connect 4 decision logic in here.
+
+    Args:
+        state (list): List of rows in game board, ie. the 'state' of the game.
+        Element of a row is True if your token, False if opponent's, and None if empty.
+
+    Returns:
+        int: Integer index of column to drop next token into.
+    """
+    # An example program to choose a random column for your move.
+    num_columns = len(state[0])
+    chosen_column = random.randint(0, num_columns)
+
+    return chosen_column
 
