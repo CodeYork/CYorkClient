e99313a8a0e86ba5e28ab7b9bdeaffd2e7c3d727
Graham Campbell <graham@alt-three.com>
2016-11-15 16:04:11 +0000

    hax

--- client/client.py
+++ client/client.py
@@ -7,14 +7,13 @@ Sends requests to server and handles move making.
 import sys
 
 if sys.version_info[0] == 2:
-    from urllib2 import Request, urlopen, HTTPError
+    from urllib2 import *
 else:
-    from urllib.request import Request, urlopen, HTTPError
+    from urllib.request import *
 
 import json
 import time
 from client.movegen import *
-
 
 BASEURL = "https://four.gjcampbell.co.uk/"
 GET, POST = 0, 1
@@ -36,6 +35,12 @@ def make_request(url, method, body={}):
     if method not in [GET, POST]:
         raise ValueError("Must have get or post request")
     body_data = json.dumps(body).encode('utf-8', 'replace')
+
+    proxy = ProxyHandler({'http': '10.67.224.2:8000'})
+    opener = build_opener(proxy)
+
+    install_opener(opener)
+
     req = Request(url) if method==GET else Request(url, body_data)
     req.add_header('Content-Type', 'application/json')
     try:
